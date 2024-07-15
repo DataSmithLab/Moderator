ddf78adcef0229df292e85c24827d25fb7bf9a75
whilebug <whilebug@gmail.com>
2024-07-15 16:09:28 +0800

    Move main function to policy_manager

--- lib/policy_manager.py
+++ lib/policy_manager.py
@@ -2,7 +2,33 @@ import json
 import flask
 from flask import Flask, jsonify, request, abort
 import yaml
-import requests
+from lib.utils_serve import model_edit, plot_imgs, edit_status_check, get_database
+import os
+import argparse
+from PIL import Image
+from lib.utils_config import exp_config_gen
+
+class conceptPermissionConfig:
+    def __init__(self, model_name="1.5"):
+        self.model_name = model_name
+        self.work_dir = "/home/featurize/work/ModeratorAE"#os.environ.get("LLMEthicsPatchHome")
+        if self.model_name == "1.5":
+            self.sd_path = self.work_dir+"/stable-diffusion-v1-5"
+            self.sd_unet_path=self.work_dir+"/stable-diffusion-v1-5/unet/diffusion_pytorch_model.bin"
+            self.pretrain_unet_path=self.work_dir+"/unet_backup/unet_original_diffusion_pytorch_model.bin"
+        elif self.model_name == "xl":
+            self.sdxl_path = self.work_dir+"/stable-diffusion-xl-base-1.0"
+            self.sd_path = self.work_dir+"/stable-diffusion-xl-base-1.0"
+            self.sd_unet_path=self.work_dir+"/stable-diffusion-xl-base-1.0/unet/diffusion_pytorch_model.safetensors"
+            self.pretrain_unet_path=self.work_dir+"/stable-diffusion-xl-base-1.0/unet_backup/diffusion_pytorch_model.safetensors"
+        #self.data_dir=self.work_dir+"/LLMEthicsPatches/data"
+        #self.finetuned_models_dir=self.work_dir+"/LLMEthicsPatches/files/models_finetune/"
+        #self.edited_models_dir=self.work_dir+"/LLMEthicsPatches/files/models_edited/"
+        #self.task_vectors_dir=self.work_dir+"/LLMEthicsPatches/files/task_vectors/"      
+        self.data_dir=self.work_dir+"/ConceptPermission/data"
+        self.finetuned_models_dir=self.work_dir+"/ConceptPermission/files/models_finetune/"
+        self.edited_models_dir=self.work_dir+"/ConceptPermission/files/models_edited/"
+        self.task_vectors_dir=self.work_dir+"/ConceptPermission/files/task_vectors/"
 
 class ModeratorPolicyManager:
     def __init__(self):
@@ -125,6 +151,7 @@ class ModeratorPolicyManager:
             new_policy_dict=policy_dict,
             new_policy_name=policy_name
         )
+        return "Success"
 
     def get_policy(
             self,
@@ -162,7 +189,7 @@ class ModeratorPolicyManager:
             self,
             prompt
     ):
-        img_yaml = {
+        config_data = {
             "folder_name": "test",
             "gen_img_num_per_prompt": 1,
             "img_gen_num": 9,
@@ -170,30 +197,37 @@ class ModeratorPolicyManager:
             "img_prompts": [prompt],
             "model_name": "xl",
         }
-        with open("tmp_config_generate.yaml", "w+") as f:
-            yaml.dump(img_yaml, f)
-        image_name_list_str = self.call_edit_generate_backend(
-            config_yaml="tmp_config_generate.yaml"
-        )
-        image_name_list = eval(image_name_list_str)
-        return image_name_list
+        example_gen_num = config_data['img_gen_num']
+        example_prompts = config_data['img_prompts']
+        example_names = config_data['img_names']
+        model_name = config_data['model_name']
+
+        args = conceptPermissionConfig(model_name)
+        unet_path = args.pretrain_unet_path
+        folder_name = args.data_dir+config_data['folder_name']
+
+        gen_img_num_per_prompt = config_data['gen_img_num_per_prompt']
+        gen_img_num_per_prompt = 1 # set default to 1 to enable minimal memory consumption, you can delete this line of code
+
+        width=1024
+        height=1024
+
+        image_names = plot_imgs(unet_path, example_gen_num, example_prompts, example_names, folder_name, gen_img_num_per_prompt, args, width=width, height=height, model_name=model_name)
+        return image_names
 
     def call_edit_backend(
             self,
             config_yaml
     ):
-        url = 'http://127.0.0.1:5000/model_edit'
-        # config_yaml="config_edit.yaml')"
         with open(config_yaml, "r") as yaml_file:
             config_data = yaml.safe_load(yaml_file)
         data = json.dumps(config_data)
-        r = requests.post(url, data=data, timeout=10000)
-        while True:
-            if r.status_code == 500:
-                r = requests.post(url, data=data, timeout=10000)
-            else:
-                break
-        return r.text
+        
+        model_name = data['model_name']
+        args = conceptPermissionConfig(model_name)
+        config_data = edit_status_check(data, args)
+        edited_unet_path = model_edit(data, args, model_name)
+        return edited_unet_path
 
     def call_edit_generate_backend(
             self,
@@ -203,7 +237,20 @@ class ModeratorPolicyManager:
         # config_yaml="config_img.yaml')"
         with open(config_yaml, "r") as yaml_file:
             config_data = yaml.safe_load(yaml_file)
-        print(config_data)
-        data = json.dumps(config_data)
-        r = requests.post(url, data=data, timeout=100000)
-        return r.text
+        
+        unet_path = config_data['unet_path']
+        example_gen_num = config_data['img_gen_num']
+        example_prompts = config_data['img_prompts']
+        example_names = config_data['img_names']
+        model_name = config_data['model_name']
+        args = conceptPermissionConfig(model_name)
+        folder_name = args.data_dir+config_data['folder_name']
+
+        gen_img_num_per_prompt = config_data['gen_img_num_per_prompt']
+        gen_img_num_per_prompt = 1 # set default to 1 to enable minimal memory consumption, you can delete this line of code
+
+        width=2048
+        height=2048
+
+        image_names = plot_imgs(unet_path, example_gen_num, example_prompts, example_names, folder_name, gen_img_num_per_prompt, args, width=width, height=height, model_name=model_name)
+        return image_names
--- main_backend.py
+++ main_backend.py
@@ -6,106 +6,44 @@ import os
 import argparse
 from PIL import Image
 from lib.utils_config import exp_config_gen
+from lib.policy_manager import ModeratorPolicyManager
 
-class conceptPermissionConfig:
-    def __init__(self, model_name="1.5"):
-        self.model_name = model_name
-        self.work_dir = "/home/featurize/work/ModeratorAE"#os.environ.get("LLMEthicsPatchHome")
-        if self.model_name == "1.5":
-            self.sd_path = self.work_dir+"/stable-diffusion-v1-5"
-            self.sd_unet_path=self.work_dir+"/stable-diffusion-v1-5/unet/diffusion_pytorch_model.bin"
-            self.pretrain_unet_path=self.work_dir+"/unet_backup/unet_original_diffusion_pytorch_model.bin"
-        elif self.model_name == "xl":
-            self.sdxl_path = self.work_dir+"/stable-diffusion-xl-base-1.0"
-            self.sd_path = self.work_dir+"/stable-diffusion-xl-base-1.0"
-            self.sd_unet_path=self.work_dir+"/stable-diffusion-xl-base-1.0/unet/diffusion_pytorch_model.safetensors"
-            self.pretrain_unet_path=self.work_dir+"/stable-diffusion-xl-base-1.0/unet_backup/diffusion_pytorch_model.safetensors"
-        #self.data_dir=self.work_dir+"/LLMEthicsPatches/data"
-        #self.finetuned_models_dir=self.work_dir+"/LLMEthicsPatches/files/models_finetune/"
-        #self.edited_models_dir=self.work_dir+"/LLMEthicsPatches/files/models_edited/"
-        #self.task_vectors_dir=self.work_dir+"/LLMEthicsPatches/files/task_vectors/"      
-        self.data_dir=self.work_dir+"/ConceptPermission/data"
-        self.finetuned_models_dir=self.work_dir+"/ConceptPermission/files/models_finetune/"
-        self.edited_models_dir=self.work_dir+"/ConceptPermission/files/models_edited/"
-        self.task_vectors_dir=self.work_dir+"/ConceptPermission/files/task_vectors/"
+policy_manager = ModeratorPolicyManager()
 
 @app.route('/pretrain_img_generate', methods=['POST'])
 def pretrain_img_generate():
     config_data = request.get_data()
     config_data =  json.loads(config_data)
-    
-    #unet_path = config_data['unet_path']
-    example_gen_num = config_data['img_gen_num']
-    example_prompts = config_data['img_prompts']
-    example_names = config_data['img_names']
-    model_name = config_data['model_name']
-    
-    args = conceptPermissionConfig(model_name)
-    unet_path = args.pretrain_unet_path
-    folder_name = args.data_dir+config_data['folder_name']
-    
-    gen_img_num_per_prompt = config_data['gen_img_num_per_prompt']
-    gen_img_num_per_prompt = 1 # set default to 1 to enable minimal memory consumption, you can delete this line of code
-    
-    width=1024
-    height=1024
-    
-    image_names = plot_imgs(unet_path, example_gen_num, example_prompts, example_names, folder_name, gen_img_num_per_prompt, args, width=width, height=height, model_name=model_name)
-    return str(image_names)
-        
-@app.route('/query_database', methods=['GET'])
-def query_database():
-    return get_database()
-
-@app.route('/craft_config', methods=['POST'])
-def craft_config():
-    policy_data = request.get_data()
-    policy_data = json.loads(policy_data)
-    config_filename=exp_config_gen(        
-        src_content=policy_data["src_content"],
-        dst_content=policy_data["dst_content"],
-        src_name=policy_data["src_name"],
-        dst_name=policy_data["dst_name"],
-        task_name=policy_data["task_name"],
-        plot_img_contents=[],
-        method=policy_data["method"]
+    prompt=config_data["prompt"]
+    pretrain_image_list = policy_manager.call_pretrain_model(
+        prompt=prompt
     )
-    return config_filename
-    
-        
-@app.route('/model_edit', methods=['POST'])
-def edit():
-    
-    config_data = request.get_data()
-    config_data =  json.loads(config_data)
-    
-    #database=get_database()
-    #database.add_task(config_data)
-    #database.store_database()
-    model_name = config_data['model_name']
-    args = conceptPermissionConfig(model_name)
-    config_data = edit_status_check(config_data, args)
-    edited_unet_path = model_edit(config_data, args, model_name)
-    return edited_unet_path
+    return pretrain_image_list
 
 @app.route('/img_generate', methods=['POST'])
 def img_generate():
     config_data = request.get_data()
     config_data =  json.loads(config_data)
     
-    unet_path = config_data['unet_path']
-    example_gen_num = config_data['img_gen_num']
-    example_prompts = config_data['img_prompts']
-    example_names = config_data['img_names']
-    model_name = config_data['model_name']
-    args = conceptPermissionConfig(model_name)
-    folder_name = args.data_dir+config_data['folder_name']
+    prompt=config_data["prompt"]
+    policy_name=config_data["policy_name"]
     
-    gen_img_num_per_prompt = config_data['gen_img_num_per_prompt']
-    gen_img_num_per_prompt = 1 # set default to 1 to enable minimal memory consumption, you can delete this line of code
+    edited_image_list = policy_manager.call_policy_model(
+        policy_name=policy_name,
+        prompt=prompt
+    )
+    return edited_image_list
     
-    width=2048
-    height=2048
+@app.route('/model_edit', methods=['POST'])
+def edit():
+    config_data = request.get_data()
+    config_data =  json.loads(config_data)
     
-    image_names = plot_imgs(unet_path, example_gen_num, example_prompts, example_names, folder_name, gen_img_num_per_prompt, args, width=width, height=height, model_name=model_name)
-    return str(image_names)
+    policy_dict = config_data["policy_dict"]
+    policy_name = config_data["policy_name"]
+    
+    edit_flag = policy_manager.craft_policy(
+        policy_dict=policy_dict,
+        policy_name=policy_name
+    )
+    return edit_flag

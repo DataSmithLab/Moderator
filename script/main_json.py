b9150120c850cc48eeae169a2cafc132ec4edc57
wangpeiran.bug <wangpeiran.bug@bytedance.com>
2025-07-29 16:11:49 +0800

    delete previous files

--- script/main_json.py
+++
@@ -1,9 +0,0 @@
-import yaml
-import json
-with open('/root/autodl-fs/LLMEthicsPatches/configs/permission_interfaces_setting.yaml', "r") as yaml_file:
-    yaml_data = yaml.safe_load(yaml_file)
-idx = 1
-category_map = {}
-for word_type, word_list in yaml_data.items():
-    category_map[word_type]=word_list
-print(category_map)
--- script/run.sh
+++
@@ -1,2 +0,0 @@
-#./run_exp_2.sh
-./run_exp_4.sh
